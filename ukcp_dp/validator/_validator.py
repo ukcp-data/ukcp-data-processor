7487e771fd067a71add7cf1997c680be53f316e3
antony-wilson <antoyn.wilson@stfc.ac.uk>
2017-11-08 12:46:35 +0000

    add more validation

--- ukcp_dp/validator/_validator.py
+++ ukcp_dp/validator/_validator.py
@@ -5,6 +5,8 @@ class Validator():
     def validate(self, input_data):
         self.input_data = input_data
         self._validate_spatial_rep()
+        self._validate_show_probability_levels()
+        self._validate_time_slice()
 
         return self.input_data
 
@@ -34,3 +36,46 @@ class Validator():
                 self.input_data.set_value(
                     InputType.SPATIAL_REPRESENTATION,
                     self.input_data.get_area_type())
+
+    def _validate_show_probability_levels(self):
+        try:
+            show_probability_levels = self.input_data.get_value(
+                InputType.SHOW_PROBABILITY_LEVELS)
+            if not isinstance(show_probability_levels, bool):
+                raise Exception("Invalid {value_type}: {value}.".format(
+                    value_type=InputType.SHOW_PROBABILITY_LEVELS,
+                    value=show_probability_levels))
+
+        except KeyError:
+            # Not set, lets be kind and set it to False
+            self.input_data.set_value(InputType.SHOW_PROBABILITY_LEVELS, False)
+
+    def _validate_time_slice(self):
+        try:
+            year = self.input_data.get_value(InputType.YEAR)
+        except KeyError:
+            year = None
+
+        try:
+            year_min = self.input_data.get_value(InputType.YEAR_MINIMUM)
+        except KeyError:
+            year_min = None
+
+        try:
+            year_max = self.input_data.get_value(InputType.YEAR_MAXIMUM)
+        except KeyError:
+            year_max = None
+
+        if (year_max is not None and year_min is None):
+            raise Exception("{min} must be provided when {max} is set".format(
+                min=InputType.YEAR_MINIMUM, max=InputType.YEAR_MAXIMUM))
+
+        if (year_min is not None and year_max is None):
+            raise Exception("{max} must be provided when {min} is set".format(
+                min=InputType.YEAR_MINIMUM, max=InputType.YEAR_MAXIMUM))
+
+        if (year is not None and year_max is not None):
+            raise Exception("{year} cannot be set at the same time as {min} "
+                            "and {max}".format(year=InputType.YEAR,
+                                               min=InputType.YEAR_MINIMUM,
+                                               max=InputType.YEAR_MAXIMUM))
