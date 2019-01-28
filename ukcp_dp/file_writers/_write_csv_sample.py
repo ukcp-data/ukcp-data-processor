0620364d3ccb773766d0a933611d48d2d539675a
antony-wilson <antoyn.wilson@stfc.ac.uk>
2019-01-28 11:58:54 +0000

    Fix issue Sampling 'select all' gives an error: TypeError: only size-1 arrays can be converted to Python scalars

--- ukcp_dp/file_writers/_write_csv_sample.py
+++ ukcp_dp/file_writers/_write_csv_sample.py
@@ -1,5 +1,6 @@
 import logging
 
+import iris
 from ukcp_dp.constants import InputType
 from ukcp_dp.file_writers._base_csv_writer import BaseCsvWriter
 from ukcp_dp.file_writers._utils import round_variable
@@ -21,27 +22,67 @@ class SampleCsvWriter(BaseCsvWriter):
         """
 
         # add the label to the header
-        self.header.append('sample id')
+        if self.input_data.get_value(InputType.TIME_PERIOD) == 'all':
+            self.header.append('Date')
+        else:
+            self.header.append('sample id')
+
         key_list = []
 
         for i, cube in enumerate(self.cube_list):
-            variable = self.input_data.get_value_label(
-                InputType.VARIABLE)[i].encode('utf-8')
-            # add the variable label to the header
-            self.header.append(variable)
-
-            for sample_slice in cube.slices_over('sample'):
-                sample_id = int(sample_slice.coord('sample').points[0])
-                value = round_variable(self.input_data.get_value(
-                    InputType.VARIABLE)[i], sample_slice.data)
-
-                try:
-                    self.data_dict[sample_id].append(value)
-                except KeyError:
-                    key_list.append(sample_id)
-                    self.data_dict[sample_id] = [value]
+            if self.input_data.get_value(InputType.TIME_PERIOD) == 'all':
+                self._write_sample_with_date(cube, i, key_list)
+            else:
+                self._write_sample(cube, i, key_list)
 
         output_data_file_path = self._get_full_file_name()
         self._write_data_dict(output_data_file_path, key_list)
 
         return [output_data_file_path]
+
+    def _write_sample(self, cube, i, key_list):
+        # add the variable label to the header
+        self.header.append(self.input_data.get_value_label(
+            InputType.VARIABLE)[i].encode('utf-8'))
+
+        for sample_slice in cube.slices_over('sample'):
+            sample_id = int(sample_slice.coord('sample').points[0])
+            value = round_variable(self.input_data.get_value(
+                InputType.VARIABLE)[i], sample_slice.data)
+
+            try:
+                self.data_dict[sample_id].append(value)
+            except KeyError:
+                key_list.append(sample_id)
+                self.data_dict[sample_id] = [value]
+
+    def _write_sample_with_date(self, cube, i, key_list):
+        """
+        Write out the data, in CSV format.
+        """
+        for sample_slice in cube.slices_over('sample'):
+            sample_id = int(sample_slice.coord('sample').points[0])
+
+            var = self.input_data.get_value_label(
+                InputType.VARIABLE)[i].encode('utf-8')
+            self.header.append('{var}(sample {sample_id})'.format(
+                sample_id=sample_id, var=var))
+            self._write_time_cube(sample_slice, key_list)
+
+    def _write_time_cube(self, cube, key_list):
+        """
+        Slice the cube over 'time' and update data_dict
+        """
+        data = cube.data[:]
+        coords = cube.coord('time')[:]
+        for t in range(0, data.shape[0]):
+            value = round_variable(self.input_data.get_value(
+                InputType.VARIABLE)[0], data[t])
+            with iris.FUTURE.context(cell_datetime_objects=True):
+                time_str = coords[t].cell(
+                    0).point.strftime('%Y-%m-%d')
+            try:
+                self.data_dict[time_str].append(value)
+            except KeyError:
+                key_list.append(time_str)
+                self.data_dict[time_str] = [value]
