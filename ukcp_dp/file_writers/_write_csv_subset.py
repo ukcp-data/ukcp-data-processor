1c33a36e0f1ce7c8f8247c9ae1a7868822af2034
Antony Wilson <antony.wilson@stfc.ac.uk>
2021-07-09 16:33:13 +0100

    improve performance for bbox subset

--- ukcp_dp/file_writers/_write_csv_subset.py
+++ ukcp_dp/file_writers/_write_csv_subset.py
@@ -3,11 +3,9 @@ This module contains the SubsetCsvWriter class, which implements the _write_csv 
 from the BaseCsvWriter base class.
 
 """
-import collections
 from datetime import datetime
 import logging
 
-import iris
 import numpy as np
 from ukcp_dp.constants import AreaType, InputType, COLLECTION_PROB
 from ukcp_dp.file_writers._base_csv_writer import BaseCsvWriter, value_to_string
@@ -27,7 +25,7 @@ class SubsetCsvWriter(BaseCsvWriter):
 
     def _write_csv(self):
         """
-        Write out the data, in CSV format, associated with three maps.
+        Write out the data, in CSV format, associated with data subsets.
 
         """
         if self.input_data.get_area_type() == AreaType.BBOX:
@@ -39,6 +37,12 @@ class SubsetCsvWriter(BaseCsvWriter):
         return self._write_region_or_point_csv()
 
     def _write_x_y_csv(self):
+        """
+        Write out data that has multiple time and x and y coordinates.
+
+        One file will be written per ensemble.
+
+        """
         LOG.debug("_write_x_y_csv")
         cube = self.cube_list[0]
 
@@ -46,71 +50,60 @@ class SubsetCsvWriter(BaseCsvWriter):
         self.header.append("x-axis,Eastings (BNG)\n")
         self.header.append("y-axis,Northings (BNG)\n")
 
-        # add the x values to the header
-        x_values = ["--"]
-        write_header = True
         output_file_list = []
+
+        # add the x values to the header
+        column_headers = ["--"]
+        x_coords = cube.coord("projection_x_coordinate").points
+        for x_coord in range(0, x_coords.shape[0]):
+            column_headers.append(str(x_coords[x_coord]))
 
         # loop over ensembles
         for ensemble_slice in cube.slices_over("ensemble_member"):
             ensemble_name = ensemble_slice.coord("ensemble_member_id").points[0]
 
-            output_data_file_path = self._get_full_file_name(
-                "_{}".format(ensemble_name)
-            )
+            LOG.debug("processing ensemble %s", ensemble_name)
+
+            output_data_file_path = self._get_full_file_name(f"_{ensemble_name}")
             self._write_headers(output_data_file_path)
 
-            # loop over times
-            for time_slice in ensemble_slice.slices_over("time"):
-                time_str = time_slice.coord("time").cell(0).point.strftime("%Y-%m-%d")
-                key_list = []
+            with open(output_data_file_path, "a") as output_data_file:
+                self._write_data_block(ensemble_slice, output_data_file, column_headers)
 
-                # get the numpy representation of the sub-cube
-                data = time_slice.data
-                # get the coordinates for the sub-cube
-                y_coords = time_slice.coord("projection_y_coordinate").points
-                x_coords = time_slice.coord("projection_x_coordinate").points
-
-                # rows of data
-                for y in range(0, y_coords.shape[0]):
-                    y_coord = str(y_coords[y])
-                    # columns of data
-                    for x in range(0, x_coords.shape[0]):
-                        if write_header is True:
-                            x_values.append(str(x_coords[x]))
-
-                        value = value_to_string(data[y, x])
-                        try:
-                            self.data_dict[y_coord].append(value)
-                        except KeyError:
-                            key_list = [y_coord] + key_list
-                            self.data_dict[y_coord] = [value]
-                    write_header = False
-
-                self._write_data_block(
-                    output_data_file_path, key_list, time_str, x_values
-                )
             output_file_list.append(output_data_file_path)
 
         return output_file_list
 
-    def _write_data_block(self, output_data_file_path, key_list, time_str, x_values):
+    def _write_data_block(self, cube, output_data_file, column_headers):
         """
-        Write out the column headers and data_dict.
+        Write out the column headers and data.
 
         """
-        with open(output_data_file_path, "a") as output_data_file:
-            output_data_file.write(time_str + "\n")
-            line_out = ",".join(x_values) + "\n"
-            output_data_file.write(line_out)
-            for key in key_list:
-                line_out = "{key},{values}\n".format(
-                    key=key, values=",".join(self.data_dict[key])
+        start_time = datetime.now()
+        # get the numpy representation of the sub-cube
+        data = cube.data[:]
+        end_time = datetime.now()
+        LOG.debug("data extracted from cube in %s", end_time - start_time)
+
+        time_coords = cube.coord("time")[:]
+        y_coords = cube.coord("projection_y_coordinate")[:]
+
+        for time_ in range(0, data.shape[0]):
+            output_data_file.write(
+                f"{time_coords[time_].cell(0).point.strftime('%Y-%m-%d')}\n"
+            )
+
+            output_data_file.write(f"{','.join(column_headers)}\n")
+
+            # rows of data
+            for y_coord in range(data.shape[1] - 1, -1, -1):
+                output_data_file.write(
+                    f"{y_coords[y_coord].cell(0).point},"
+                    f"{','.join(['%s' % num for num in data[:][time_][y_coord]])}"
+                    "\n"
                 )
-                output_data_file.write(line_out)
 
-        # reset the data dict
-        self.data_dict = collections.OrderedDict()
+        LOG.debug("data written to file")
 
     def _write_csv_percentiles(self):
         """
