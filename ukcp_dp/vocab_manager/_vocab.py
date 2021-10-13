ddb72e54665bfd39e668323a12ccb2c68762792c
Antony Wilson <antony.wilson@stfc.ac.uk>
2021-10-13 10:51:36 +0100

    fix typo in vocab

--- ukcp_dp/vocab_manager/_vocab.py
+++ ukcp_dp/vocab_manager/_vocab.py
@@ -215,7 +215,7 @@ class Vocab:
         self.vocab["year"].update(_get_range(3001, 3051))
         self.vocab["year_minimum"] = _get_range(1862, 2302)
         self.vocab["year_minimum"].update(_get_range(3001, 3052))
-        self.vocab["year_maximum"] = _get_range(1862y, 2302)
+        self.vocab["year_maximum"] = _get_range(1862, 2302)
         self.vocab["year_maximum"].update(_get_range(3001, 3052))
         self.vocab["sampling_id"] = _get_range(1, 4001)
         self.vocab["random_sampling_count"] = _get_range(100, 4001)
