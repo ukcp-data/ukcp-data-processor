fdfdce40ebd7a423fdc4dc9e347aeb1975388887
Antony Wilson <antony.wilson@stfc.ac.uk>
2024-04-11 13:41:41 +0100

    Update for GWL

--- ukcp_dp/vocab_manager/_vocab.py
+++ ukcp_dp/vocab_manager/_vocab.py
@@ -108,6 +108,14 @@ class Vocab:
             "netcdf": "CF-netCDF",
             "shp": "Shapefile",
         },
+        "gwl": {
+            "gwl1": "1°C above pre-industrial",
+            "gwl1.5": "1.5°C above pre-industrial",
+            "gwl2": "2°C above pre-industrial",
+            "gwl2.5": "2.5°C above pre-industrial",
+            "gwl3": "3°C above pre-industrial",
+            "gwl4": "4°C above pre-industrial",
+        },
         "show_boundaries": {
             "none": "None",
             "country": "Country",
@@ -178,6 +186,7 @@ class Vocab:
         "data_type": "Data Type",
         "ensemble": "Members",
         "font_size": "Font Size",
+        "gwl": "Global Warming Level",
         "highlighted_ensemble_members": "Highlighted Members",
         "image_format": "Image Format",
         "image_size": "Image Size",
